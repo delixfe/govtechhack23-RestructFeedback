37fcb4df66e2f7c7ece305eb9786449e9cfabdd5
delixfe <felix.deierlein@bit.admin.ch>
2023-03-24 08:11:21 +0100

    backend: more iterations and then stuck
    This model's maximum context length is 4097 tokens. However, your messages resulted in 5088 tokens.

--- backend/Restruct.IntegrationTests/Fixtures.cs
+++ backend/Restruct.IntegrationTests/Fixtures.cs
@@ -6,12 +6,12 @@ public static class Fixtures
     private static readonly Lazy<string> _vernehmlassungBe20210331;
     
     public static string EcflVnBundesgesetzD => _ecflVnBundesgesetzD.Value;
-    public static string VernehmlassungBe20210331 => _ecflVnBundesgesetzD.Value;
+    public static string VernehmlassungBe20210331 => _vernehmlassungBe20210331.Value;
     
     static Fixtures()
     {
         _ecflVnBundesgesetzD = new Lazy<string>(() => File.ReadAllText(Path.Combine("Fixtures","2021__3", "ecfl_vn_bundesgesetz_d.pdf.txt")));
-        _vernehmlassungBe20210331 = new Lazy<string>(() => File.ReadAllText(Path.Combine("Fixtures","2021__3", "Vernehmlassung_BE_20210331")));
+        _vernehmlassungBe20210331 = new Lazy<string>(() => File.ReadAllText(Path.Combine("Fixtures","2021__3", "Vernehmlassung_BE_20210331.txt")));
     }
     
 }
--- backend/Restruct.IntegrationTests/OpenAITests/PromptTests.cs
+++ backend/Restruct.IntegrationTests/OpenAITests/PromptTests.cs
@@ -12,18 +12,38 @@ public class PromptTests
     {
         var consultation = Fixtures.EcflVnBundesgesetzD;
         var submission = Fixtures.VernehmlassungBe20210331;
-        var api = new OpenAIClient();
-        var chatPrompts = new List<ChatPrompt>
+
+        var prompts = new List<ChatPrompt>
         {
-            new ChatPrompt("system", "You are a helpful assistant."),
-            new ChatPrompt("user", $"Der folgende Text ist ein Entwurf für die Revision eines Gesetzes."),
-            new ChatPrompt("user", consultation),
-            new ChatPrompt("user", $"Dazu haben wir die folgende Stellungnahme erhalten."),
-            new ChatPrompt("user", submission),
-            new ChatPrompt("user", $"Fasse die Stellungnahme zusammen."),
-        };
+            new ChatPrompt("system", "Du bist ein präziser Assistent und drückst Dich so kurz wie möglich aus."),
+            new ChatPrompt("user", $"""
+                Ich stelle die folgenden 2 Datein und ihre Dateinamen zur Verfügung.
         
-        var chatRequest = new ChatRequest(chatPrompts);
+                Datei: entwurf.txt
+                Inhalt:
+                {consultation}
+            
+                Datei: stellungnahme.txt
+                Inhalt:
+                {submission}
+               
+                Fasse den Inhalt der Datei entwurf.txt in 1 Satz zusammen.
+                """),
+            new ChatPrompt("assistant","""Die Datei "entwurf.txt" enthält den Entwurf eines Bundesgesetzes."""),
+            new ChatPrompt("user", """Fasse den Inhalt der Datei "stellungnahme.txt" in 1 Satz zusammen."""),
+            
+        };
+
+       await Prompt(prompts, number: null);
+    }
+
+
+    public async Task<ChatResponse> Prompt(IEnumerable<ChatPrompt> chatPrompts, int? number = null )
+    {
+        var api = new OpenAIClient();
+        
+        var chatRequest = new ChatRequest(chatPrompts, user: GetType().FullName, temperature: 0, number: number);
+        
         var result = await api.ChatEndpoint.GetCompletionAsync(chatRequest)!;
         
         TestContext.WriteLine();
@@ -38,5 +58,7 @@ public class PromptTests
         TestContext.WriteLine();
         Assert.IsNotNull(result);
         Assert.That(result.Object, Is.EqualTo("chat.completion"));
+        
+        return result;
     }
 }
